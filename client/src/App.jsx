4609fd3a88f088e80988bf3b34c06d149f70973c
Ananya Bera <techananyaofficial@gmail.com>
2026-07-12 14:46:01 +0530

    menubar added to thedashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import LoginForm from "./components/loginForm"
 import Dashboard from "./components/dashboard"
 import VehicleRegistry from "./components/vehicalRegistry"
-import { useState } from "react"
+import ManagementPage from "./components/managementPage"
 import SignupForm from "./components/signupForm"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path="/" element={<LoginForm />} />
       <Route path="/signup" element={<SignupForm />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/vehicle-registry" element={<VehicleRegistry />} />
+      <Route path="/driver-management" element={<ManagementPage title="Driver Management" />} />
+      <Route path="/trip-management" element={<ManagementPage title="Trip Management" />} />
+      <Route path="/maintenance" element={<ManagementPage title="Maintenance" />} />
+      <Route path="/fuel-expenses" element={<ManagementPage title="Fuel & Expense Management" />} />
+      <Route path="/reports-analytics" element={<ManagementPage title="Reports & Analytics" />} />
     </Routes>
   );
 }
