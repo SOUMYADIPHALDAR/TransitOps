80390dfa9813ef1cdbd4749b978b909dda4289cb
Padmalochan-812 <sahoopadma812@gmail.com>
2026-07-12 11:56:11 +0530

    Add login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 
-
+import LoginForm from "./components/loginForm"
+import { useState } from "react"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>Hello world</div>
+   <div>
+    <LoginForm />
+   </div>
   )
 }
 
