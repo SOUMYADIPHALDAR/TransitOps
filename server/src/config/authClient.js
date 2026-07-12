7660792ac9f9c93aa8d7f8b96247edc1d55aa36a
Soumya <soumyadiphaldar3@gmail.com>
2026-07-12 12:12:16 +0530

    implement better auth

---
+++ server/src/config/authClient.js
@@ -0,0 +1,13 @@
+// lib/auth-client.js
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: import.meta.env.VITE_BETTER_AUTH_URL
+});
+
+export const {
+  signIn,
+  signUp,
+  signOut,
+  useSession,
+} = authClient;
