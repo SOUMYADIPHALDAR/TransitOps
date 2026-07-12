fc6c39bf7d7eed0ca3db7a1d8cb912f37ab16364
sesneha190529 <das05sneha@gmail.com>
2026-07-12 10:56:03 +0530

    Add Error Handling

---
+++ server/utills/apiError.js
@@ -0,0 +1,19 @@
+class apiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = null,
+    stack = "",
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode || 500;
+    this.message = message;
+    this.error = error;
+    this.success = false;
+    this.data = null;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default apiError;
---
+++ server/utills/apiResponse.js
@@ -0,0 +1,10 @@
+class apiResponse {
+    constructor(statusCode, data, message = "Request successful") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
+
+export default apiResponse;
