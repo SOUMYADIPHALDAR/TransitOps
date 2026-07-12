ea5e29380a86ba662df0fe11c42513ac96f074ef
sesneha190529 <das05sneha@gmail.com>
2026-07-12 11:31:50 +0530

    Driver's controller is added

---
+++ server/src/controller/driver.controller.js
@@ -0,0 +1,87 @@
+import asyncHandler from "../utils/asyncHandler";
+import apiError from "../utils/apiError";
+import apiResponse from "../utils/apiResponse";
+
+const newDriver = asyncHandler(async (req, res) => {
+  const {
+    name,
+    contactNumber,
+    licenseNumber,
+    licenseCategory,
+    licenseExpiryDate,
+  } = req.body;
+  if (
+    !name ||
+    contactNumber ||
+    licenseCategory ||
+    licenseNumber ||
+    licenseExpiryDate
+  ) {
+    throw new apiError(404, "All these fields are required");
+  }
+
+  const existingDriver = await prisma.driver.findUnique({
+    where: { licenseNumber },
+  });
+
+  if (existingDriver) {
+    throw new apiError(404, "License already exists");
+  }
+
+  const createDriver = await prisma.driver.create({
+    data: {
+      name,
+      contactNumber,
+      licenseNumber,
+      licenseCategory,
+      licenseExpiryDate,
+      createdAt: new Date(),
+    },
+  });
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, createDriver, "New driver created successfully"),
+    );
+});
+
+const editDriver = asyncHandler(async (req, res) => {
+  const { name, contactNumber } = req.body;
+  const { id } = req.params;
+
+  if (!name || !contactNumber) {
+    throw new apiError(400, "All fields are requires");
+  }
+
+  const driver = await prisma.driver.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  if (!driver) {
+    throw new apiError(404, "Driver doesn't exist");
+  }
+
+  const updateDriver = await prisma.driver.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      contactNumber,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        updateDriver,
+        "Driver's information has been updated successfully",
+      ),
+    );
+});
+
